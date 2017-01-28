6d47fc415f5ecb7b496467bfc91da3935287947e
Oguzhan YAGCI <oyagci@student.42.fr>
2017-01-28 13:53:29 +0100

    Jump from word to word with ALT+ARROW LEFT/RIGHT

--- srcs/ft_get_command_line.c
+++ srcs/ft_get_command_line.c
@@ -6,7 +6,7 @@
 /*   By: oyagci <oyagci@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/07 12:51:23 by oyagci            #+#    #+#             */
-/*   Updated: 2017/01/28 09:59:44 by oyagci           ###   ########.fr       */
+/*   Updated: 2017/01/28 13:52:20 by oyagci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,12 @@
 #include <termios.h>
 #include <minishell.h>
 #include <sys/ioctl.h>
+
+void			set_pos(t_pos *pos, int x, int y)
+{
+	pos->x = x;
+	pos->y = y;
+}
 
 int				put_tty(int c)
 {
@@ -106,12 +112,32 @@ void			add_char(t_c **line, char c)
 	(*line)->prev = new;
 }
 
+void			delete_char(t_c **line)
+{
+	if ((*line)->prev)
+	{
+		(*line)->prev = (*line)->prev->prev;
+		if ((*line)->prev)
+			(*line)->prev->next = (*line);
+	}
+}
+
 void			move_cur_left(t_c **line)
 {
 	if ((*line)->prev)
 	{
 		(*line)->cursor_on = 0;
 		*line = (*line)->prev;
+		(*line)->cursor_on = 1;
+	}
+}
+
+void			move_cur_right(t_c **line)
+{
+	if ((*line)->next)
+	{
+		(*line)->cursor_on = 0;
+		*line = (*line)->next;
 		(*line)->cursor_on = 1;
 	}
 }
@@ -130,8 +156,44 @@ void			set_raw(struct termios *save)
 
 void			put_cursor(t_c *line, t_pos *cur)
 {
-	(void)cur;
-	(void)line;
+	const struct winsize	win = get_winsize();
+
+	line = get_first(line);
+	reset_cursor(cur);
+	while (line && !line->cursor_on)
+	{
+		if (cur->x >= win.ws_col - 1)
+		{
+			ft_putchar('\n');
+			cur->x = 0;
+			cur->y += 1;
+		}
+		tputs(tgetstr("nd", NULL), 1, &put_tty);
+		cur->x += 1;
+		line = line->next;
+	}
+}
+
+void			set_index(t_c *line)
+{
+	const struct winsize	win = get_winsize();
+	t_pos					p;
+
+	line = get_first(line);
+	p.x = 0;
+	p.y = 0;
+	while (line)
+	{
+		if (p.x >= win.ws_col - 1)
+		{
+			p.x = 0;
+			p.y += 1;
+		}
+		line->pos.x = p.x;
+		line->pos.y = p.y;
+		p.x += 1;
+		line = line->next;
+	}
 }
 
 void			print_line(t_c *line, t_pos *cur)
@@ -142,7 +204,7 @@ void			print_line(t_c *line, t_pos *cur)
 	clear_line(cur);
 	while (line && line->c != 0)
 	{
-		if (cur->x > win.ws_col - 1)
+		if (cur->x >= win.ws_col - 1)
 		{
 			ft_putchar('\n');
 			cur->x = 0;
@@ -153,14 +215,42 @@ void			print_line(t_c *line, t_pos *cur)
 		line = line->next;
 	}
 	put_cursor(line, cur);
+	set_index(line);
+}
+
+void			jmp_word_back(t_c **line)
+{
+	move_cur_left(line);
+	while ((*line)->prev && ft_isspace((*line)->prev->c))
+		move_cur_left(line);
+	while ((*line)->prev && !ft_isspace((*line)->prev->c))
+		move_cur_left(line);
+}
+
+void			jmp_word_forward(t_c **line)
+{
+	move_cur_right(line);
+	while ((*line)->next && ft_isspace((*line)->next->c) && (*line)->next->c != 0)
+		move_cur_right(line);
+	while ((*line)->next && !ft_isspace((*line)->next->c) && (*line)->next->c != 0)
+		move_cur_right(line);
+	move_cur_right(line);
 }
 
 void			parse_buffer(t_c **line, char *buffer, int buf_size)
 {
-	if (buf_size == 1 && (ft_isprint(buffer[0]) || ft_isspace(buffer[0])))
+	if (buf_size == 1 && (ft_isprint(buffer[0]) || ft_isspace(buffer[0])) && buffer[0] != '\n')
 		add_char(line, buffer[0]);
 	else if (buf_size == 3 && buffer[0] == 27 && buffer[1] == 91 && buffer[2] == 68)
 		move_cur_left(line);
+	else if (buf_size == 3 && buffer[0] == 27 && buffer[1] == 91 && buffer[2] == 67)
+		move_cur_right(line);
+	else if (buf_size == 1 && buffer[0] == 127)
+		delete_char(line);
+	else if (buf_size == 4 && buffer[0] == 27 && buffer[1] == 27 && buffer[2] == 91 && buffer[3] == 68)
+		jmp_word_back(line);
+	else if (buf_size == 4 && buffer[0] == 27 && buffer[1] == 27 && buffer[2] == 91 && buffer[3] == 67)
+		jmp_word_forward(line);
 }
 
 int				ft_get_command_line(char **command_line)
@@ -172,6 +262,7 @@ int				ft_get_command_line(char **command_line)
 
 	pos.x = 0;
 	pos.y = 0;
+	set_pos(&pos, 0, 0);
 	line = new_c(0);
 	line->cursor_on = 1;
 	set_raw(NULL);
@@ -183,6 +274,7 @@ int				ft_get_command_line(char **command_line)
 		if (buffer[0] == '\n')
 			break ;
 	}
+	ft_putchar('\n');
 	*command_line = to_str(line);
 	return (0);
 }
